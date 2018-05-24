efaf098a9ac85a0365c7226d9deea20fa3b16850
ortizk <ortizk@live.com>
2018-05-23 17:09:26 -0700

    removed cat favicon

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
+
+
 class Login extends Component {
 	constructor(props){
 		super(props);
